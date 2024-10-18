f8175b90ebbd455ebf0aee11bb8780a4f7e7694f
MarcosAlves90 <MarcosAlves25@proton.me>
2024-10-18 17:08:21 -0300

    :sparkles: feat: refactor PaginaServicos.jsx code

--- src/components/PaginaServicos.jsx
+++ src/components/PaginaServicos.jsx
@@ -7,6 +7,145 @@ import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import PropTypes from "prop-types";
 
+const GridComponent = ({ items, onItemClick }) => (
+    <div className="grid-container">
+        {items.map(item => (
+            <div key={item.id} className="grid-item" style={{ '--hover-color': item.color }} onClick={() => onItemClick(item)}>
+                <img src={item.image} alt={item.name} className="grid-image" />
+                <p className="grid-name">{item.name}</p>
+                <i className="bi bi-eye-fill"></i>
+            </div>
+        ))}
+    </div>
+);
+
+GridComponent.propTypes = {
+    items: PropTypes.array.isRequired,
+    onItemClick: PropTypes.func.isRequired
+}
+
+const ContactItem = ({ type, contact }) => {
+    const iconClasses = {
+        "WhatsApp": "bi bi-whatsapp",
+        "Telefone": "bi bi-telephone",
+        "Instagram": "bi bi-instagram",
+        "Email": "bi bi-envelope",
+        "Other": ""
+    };
+
+    const handleInstagramClick = (instagramHandle) => {
+        window.open(`https://instagram.com/${instagramHandle}`, '_blank', 'noopener,noreferrer');
+    };
+
+    return (
+        <p className="p-paragraph contact" onClick={type === "Instagram" ? () => handleInstagramClick(contact.replace("@", "")) : null} style={{ cursor: type === "Instagram" ? 'pointer' : 'default' }}>
+            <i className={iconClasses[type] || iconClasses["Other"]}></i> {contact}
+        </p>
+    );
+};
+
+ContactItem.propTypes = {
+    type: PropTypes.string.isRequired,
+    contact: PropTypes.string.isRequired
+};
+
+const formatContact = (contact) => {
+    const contactTypes = {
+        "WhatsApp:": "WhatsApp",
+        "Telefone:": "Telefone",
+        "Instagram:": "Instagram",
+        "Email:": "Email"
+    };
+
+    return contact.split('|').map((c, index) => {
+        c = c.trim();
+        for (const [prefix, type] of Object.entries(contactTypes)) {
+            if (c.startsWith(prefix)) {
+                return <ContactItem key={index} type={type} contact={c.replace(prefix, "").trim()} />;
+            }
+        }
+        return <ContactItem key={index} type="Other" contact={c} />;
+    });
+};
+
+const PopupContent = ({ item, onClose, mobileSelectedExtra, setMobileSelectedExtra, selectedExtra, setSelectedExtra }) => {
+    const handleRightArrowButtonClick = (extras) => {
+        if (mobileSelectedExtra !== extras - 1) setMobileSelectedExtra(mobileSelectedExtra + 1);
+    };
+
+    const handleLeftArrowButtonClick = () => {
+        if (mobileSelectedExtra !== -1) setMobileSelectedExtra(mobileSelectedExtra - 1);
+    };
+
+    const returnRightBoxImageSrc = () => {
+        return item.extra[selectedExtra];
+    };
+
+    const handleRightBoxImageClick = () => {
+        if ((item.extra.length - 1) > selectedExtra) {
+            setSelectedExtra(selectedExtra + 1);
+        } else {
+            setSelectedExtra(0);
+        }
+    };
+
+    return (
+        <Popup open closeOnDocumentClick onClose={onClose}>
+            <div className={`popup-content inside ${item.extra.length > 0 ? "" : "one"}`} style={{ '--item-color': item.color }}>
+                <i className="bi bi-eye-fill"></i>
+                <div className="box">
+                    <div className="left-box">
+                        {mobileSelectedExtra === -1 ? (
+                            <>
+                                <img src={item.image} alt={item.name} className="popup-image" />
+                                <h3 className="h3-title">{item.name}</h3>
+                                <p className="p-paragraph description">{item.description}</p>
+                                {formatContact(item.contact)}
+                            </>
+                        ) : (
+                            <div className="right-box mobile">
+                                <img src={item.extra[mobileSelectedExtra]} alt={item.name} className="popup-extra" />
+                            </div>
+                        )}
+                        <div className="box-buttons popup">
+                            {mobileSelectedExtra > -1 && (
+                                <button className="button-arrow" onClick={handleLeftArrowButtonClick}>
+                                    <i className="bi bi-arrow-left"></i>
+                                </button>
+                            )}
+                            <button className={`button-close ${mobileSelectedExtra > -1 && mobileSelectedExtra < (item.extra.length - 1) ? "center" : mobileSelectedExtra === (item.extra.length - 1) && item.extra.length > 0 ? "small-left-margin" : item.extra.length > 0 ? "small" : "center"}`} onClick={onClose}>
+                                Fechar
+                            </button>
+                            {item.extra && mobileSelectedExtra < (item.extra.length - 1) && (
+                                <button className="button-arrow" onClick={() => handleRightArrowButtonClick(item.extra.length)}>
+                                    <i className="bi bi-arrow-right"></i>
+                                </button>
+                            )}
+                        </div>
+                    </div>
+                    {item.extra.length > 0 && (
+                        <div className="right-box">
+                            {item.extra.length > 1 && (
+                                <i className="bi bi-hand-index-thumb-fill"></i>
+                            )}
+                            <img src={returnRightBoxImageSrc()} alt={item.name} onClick={handleRightBoxImageClick} className={`popup-extra ${item.extra.length < 2 ? "no-click" : ""}`} />
+                        </div>
+                    )}
+                </div>
+            </div>
+        </Popup>
+    );
+};
+
+PopupContent.propTypes = {
+    item: PropTypes.object.isRequired,
+    onClose: PropTypes.func.isRequired,
+    mobileSelectedExtra: PropTypes.number.isRequired,
+    setMobileSelectedExtra: PropTypes.func.isRequired,
+    selectedExtra: PropTypes.number.isRequired,
+    setSelectedExtra: PropTypes.func.isRequired
+}
+
 export default function PaginaServicos() {
     const { selectedService, setSelectedService } = useContext(UserContext);
     const navigate = useNavigate();
@@ -21,18 +160,6 @@ export default function PaginaServicos() {
 
     const handleGridItemClick = (item) => setSelectedItem(item);
 
-    const GridComponent = () => (
-        <div className="grid-container">
-            {data[selectedService]?.map(item => (
-                <div key={item.id} className="grid-item" style={{ '--hover-color': item.color }} onClick={() => handleGridItemClick(item)}>
-                    <img src={item.image} alt={item.name} className="grid-image" />
-                    <p className="grid-name">{item.name}</p>
-                    <i className="bi bi-eye-fill"></i>
-                </div>
-            ))}
-        </div>
-    );
-
     const generateTitle = (title) => ({
         "comidas": "Comidas/Doces",
         "estetica": "Estética",
@@ -41,70 +168,6 @@ export default function PaginaServicos() {
         "acessorios": "Acessórios",
         "servicos-comunitarios": "Serviços Comunitários"
     }[title]);
-
-    const ContactItem = ({ type, contact }) => {
-        const iconClasses = {
-            "WhatsApp": "bi bi-whatsapp",
-            "Telefone": "bi bi-telephone",
-            "Instagram": "bi bi-instagram",
-            "Email": "bi bi-envelope",
-            "Other": ""
-        };
-
-        const handleInstagramClick = (instagramHandle) => {
-            window.open(`https://instagram.com/${instagramHandle}`, '_blank', 'noopener,noreferrer');
-        };
-
-        return (
-            <p className="p-paragraph contact" onClick={type === "Instagram" ? () => handleInstagramClick(contact.replace("@", "")) : null} style={{ cursor: type === "Instagram" ? 'pointer' : 'default' }}>
-                <i className={iconClasses[type] || iconClasses["Other"]}></i> {contact}
-            </p>
-        );
-    };
-
-    ContactItem.propTypes = {
-        type: PropTypes.string.isRequired,
-        contact: PropTypes.string.isRequired
-    };
-
-    const formatContact = (contact) => {
-        const contactTypes = {
-            "WhatsApp:": "WhatsApp",
-            "Telefone:": "Telefone",
-            "Instagram:": "Instagram",
-            "Email:": "Email"
-        };
-
-        return contact.split('|').map((c, index) => {
-            c = c.trim();
-            for (const [prefix, type] of Object.entries(contactTypes)) {
-                if (c.startsWith(prefix)) {
-                    return <ContactItem key={index} type={type} contact={c.replace(prefix, "").trim()} />;
-                }
-            }
-            return <ContactItem key={index} type="Other" contact={c} />;
-        });
-    };
-
-    const handleRightArrowButtonClick = (extras) => {
-        if (mobileSelectedExtra !== extras - 1) setMobileSelectedExtra(mobileSelectedExtra + 1);
-    };
-
-    const handleLeftArrowButtonClick = () => {
-        if (mobileSelectedExtra !== -1) setMobileSelectedExtra(mobileSelectedExtra - 1);
-    };
-
-    const returnRightBoxImageSrc = () => {
-        return selectedItem.extra[selectedExtra];
-    };
-
-    const handleRightBoxImageClick = () => {
-        if ((selectedItem.extra.length - 1) > selectedExtra) {
-            setSelectedExtra(selectedExtra + 1);
-        } else {
-            setSelectedExtra(0);
-        }
-    }
 
     useEffect(() => {
         if (!selectedItem) {
@@ -120,63 +183,21 @@ export default function PaginaServicos() {
     return (
         <main className="mainCommon services">
             <h2 className="h2-title-no-box">{generateTitle(selectedService)}</h2>
-            <GridComponent />
+            <GridComponent items={data[selectedService] || []} onItemClick={handleGridItemClick} />
             <div className="box-buttons">
                 <button className="center small" onClick={handleReturnButtonClick}>
                     <i className="bi bi-arrow-left"></i> Voltar
                 </button>
             </div>
             {selectedItem && (
-                <Popup open closeOnDocumentClick onClose={() => setSelectedItem(null)}>
-                    <div className={`popup-content inside ${selectedItem.extra.length > 0 ? "" : "one"}`}
-                         style={{'--item-color': selectedItem.color}}>
-                        <i className="bi bi-eye-fill"></i>
-                        <div className="box">
-                            <div className="left-box">
-                                {mobileSelectedExtra === -1 ? (
-                                    <>
-                                        <img src={selectedItem.image} alt={selectedItem.name} className="popup-image"/>
-                                        <h3 className="h3-title">{selectedItem.name}</h3>
-                                        <p className="p-paragraph description">{selectedItem.description}</p>
-                                        {formatContact(selectedItem.contact)}
-                                    </>
-                                ) : (
-                                    <div className="right-box mobile">
-                                        <img src={selectedItem.extra[mobileSelectedExtra]} alt={selectedItem.name}
-                                             className="popup-extra"/>
-                                    </div>
-                                )}
-                                <div className="box-buttons popup">
-                                    {mobileSelectedExtra > -1 && (
-                                        <button className="button-arrow" onClick={handleLeftArrowButtonClick}>
-                                            <i className="bi bi-arrow-left"></i>
-                                        </button>
-                                    )}
-                                    <button
-                                        className={`button-close ${mobileSelectedExtra > -1 && mobileSelectedExtra < (selectedItem.extra.length - 1) ? "center" : mobileSelectedExtra === (selectedItem.extra.length - 1) && selectedItem.extra.length > 0 ? "small-left-margin" : selectedItem.extra.length > 0 ? "small" : "center"}`}
-                                        onClick={() => setSelectedItem(null)}>
-                                        Fechar
-                                    </button>
-                                    {selectedItem.extra && mobileSelectedExtra < (selectedItem.extra.length - 1) && (
-                                        <button className="button-arrow"
-                                                onClick={() => handleRightArrowButtonClick(selectedItem.extra.length)}>
-                                            <i className="bi bi-arrow-right"></i>
-                                        </button>
-                                    )}
-                                </div>
-                            </div>
-                            {selectedItem.extra.length > 0 && (
-                                <div className="right-box">
-                                    {selectedItem.extra.length > 1 && (
-                                        <i className="bi bi-hand-index-thumb-fill"></i>
-                                    )}
-                                    <img src={returnRightBoxImageSrc()} alt={selectedItem.name}
-                                         onClick={handleRightBoxImageClick} className={`popup-extra ${selectedItem.extra.length < 2 ? "no-click" : ""}`}/>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </Popup>
+                <PopupContent
+                    item={selectedItem}
+                    onClose={() => setSelectedItem(null)}
+                    mobileSelectedExtra={mobileSelectedExtra}
+                    setMobileSelectedExtra={setMobileSelectedExtra}
+                    selectedExtra={selectedExtra}
+                    setSelectedExtra={setSelectedExtra}
+                />
             )}
         </main>
     );
